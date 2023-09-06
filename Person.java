7738c8b653a6074666df8b384ac7f04d642fcda5
hikile <140878313+hikile@users.noreply.github.com>
2023-09-06 19:37:48 +0700

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /*
@@ -9,42 +10,65 @@ import java.util.Scanner;
  *
  * @author TRAN HUYNH DUC
  */
-public class Person {
-    private String Name;
-    private int Age;
-    private String Gender;
-    public void setName(String Name){
-        this.Name=Name;
+public class Classroom {
+    private int classId;
+    private int numberOfStudent;
+    ArrayList<Student> listStudent = new ArrayList<>();
+    public void setclassId(int classId){
+        this.classId=classId;
     }
-    public String getName(){
-        return Name;
+    public void setnumberOfStudent(int numberOfStudent){
+        this.numberOfStudent=numberOfStudent;
     }
-    public void setAge(int Age){
-        this.Age=Age;
+    public int getclassId(){
+        return classId;
     }
-    public int getAge(){
-        return Age;
+    public int getnumberOfStudent(){
+        return numberOfStudent;
     }
-    public void setGender(String Gender){
-        this.Gender=Gender;
-    }
-    public String getGender(){
-        return Gender;
-    }
-    
     public void input(){
         Scanner sc = new Scanner(System.in);
-        System.out.print("nhap ten: ");
-        Name=sc.nextLine();
-        System.out.print("nhap tuoi: ");
-        Age=sc.nextInt();
-        sc.nextLine();
-        System.out.print("nhap gioi tinh: ");
-        Gender = sc.nextLine();
+        System.out.print("Nhap classID: ");
+        classId=sc.nextInt();
+        System.out.print("Nhap so luong hoc sinh: ");
+        numberOfStudent=sc.nextInt();
+        for(int i=1 ; i<=numberOfStudent ;i++){
+            Student stuDent = new Student();
+            stuDent.input();
+            listStudent.add(stuDent);
+        }
     }
     public void output(){
-        System.out.println("ten la: "+Name);
-        System.out.println("Tuoi: "+Age);
-        System.out.println("gioi tinh: "+Gender);
+        System.out.println("classID: " + classId);
+        System.out.println("so luong hoc sinh la: " + numberOfStudent);
+        for(Student student:listStudent){
+            student.output();
+        }
     }
+   public ArrayList<Student> searchByName(String name){
+       int check=0;
+        for(Student student : listStudent){
+           String Name=student.getName();
+            if(Name.contains(name)){
+                student.output();
+                check=1;
+                break;
+            }
+        }
+        if(check==0){
+            System.out.println("khong co hoc sinh muon tim");
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+        Classroom Class = new Classroom();
+        Scanner sc = new Scanner(System.in);
+        String name;
+        Class.input();
+        Class.output();
+        System.out.print("Nhap ten hoc sinh muon tim: ");
+        name=sc.nextLine();
+        Class.searchByName(name);
+    }
+    
 }
